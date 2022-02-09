e655e771ed30f795ddfdd0899951d1f7260e0653
trevin-livele <liveletrevin6@gmail.com>
2022-02-09 15:53:51 +0300

    db create

--- puppycompanyblog/core/views.py
+++ puppycompanyblog/core/views.py
@@ -1,6 +1,7 @@
 # core/views.py
 from puppycompanyblog.models import BlogPost
 from flask import render_template,request,Blueprint
+from puppycompanyblog import db
 
 core = Blueprint('core',__name__)
 
@@ -8,6 +9,7 @@ core = Blueprint('core',__name__)
 
 @core.route('/')
 def index():
+    db.create_all()
     page = request.args.get('page',1,type=int)
     blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page = page,per_page=5)
     return render_template('index.html',blog_posts=blog_posts)
